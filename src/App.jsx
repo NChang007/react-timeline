d2d59c8b1e4313bd83b97dd2422fba5cefc80cad
Nelson Chang <nelsonc924@gmail.com>
2023-09-30 08:08:09 -0400

    updates

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import TimeLine from './timeline/TimeLine'
+
+function App() {
+
+  return (
+    <div style={{width:'100%'}}>
+      <TimeLine></TimeLine>
+    </div>
+  )
+}
+
+export default App
---
+++ src/timeline/TimeLine.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './timeline.css'
+
+const TimeLine = () => {
+  return (
+    <div className='timeline'>
+        <div className='timeline__inner'>
+            <div className='inner-line'></div>
+            <div className='event' data-direction="up">Launch Date</div>
+            <div className='event' data-direction="up">Became President</div>
+        </div>
+    </div>
+  )
+}
+export default TimeLine
